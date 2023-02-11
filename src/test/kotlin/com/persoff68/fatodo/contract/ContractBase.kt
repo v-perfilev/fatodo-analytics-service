0a0e40f326222dbbfacc175cebca7354d31ea688
Vladimir Perfilev <persoff68@gmail.com>
2023-02-11 23:12:50 +0100

    TDM-198 edit contract configuration

--- src/test/kotlin/com/persoff68/fatodo/contract/ContractBase.kt
+++ src/test/kotlin/com/persoff68/fatodo/contract/ContractBase.kt
@@ -2,13 +2,17 @@ package com.persoff68.fatodo.contract
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc
 import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier
 import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMessageVerifier
-abstract class ContractBase(private val context: WebApplicationContext) {
+abstract class ContractBase {
+    @Autowired
+    lateinit var context: WebApplicationContext
+
     @BeforeEach
     fun setup() {
         RestAssuredMockMvc.webAppContextSetup(context)
