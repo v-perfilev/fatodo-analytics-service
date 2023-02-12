cb0fbb49fce8bdb3bb2aee33c225657e56a2b85e
Vladimir Perfilev <persoff68@gmail.com>
2023-02-12 20:12:55 +0100

    TDM-198 remove unneaded gson dependency

--- src/main/kotlin/com/persoff68/fatodo/ExtendedMysqlKotlinSkeletonApplication.kt
+++ src/main/kotlin/com/persoff68/fatodo/ExtendedMysqlKotlinSkeletonApplication.kt
@@ -2,10 +2,11 @@ package com.persoff68.fatodo
 
 import com.persoff68.fatodo.config.AppProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [GsonAutoConfiguration::class])
 @EnableConfigurationProperties(AppProperties::class)
 class ExtendedMysqlKotlinSkeletonApplication
 
